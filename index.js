fd3fd74c0d5812d8dcb38f128398f14175756356
Harold Saenz <joshu4nick19@gmail.com>
2025-06-04 12:36:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ function convert()
     {
         temp = Number(textbox.value);
         temp = temp * 9 / 5 + 32;
+        result.style.color = "black";
         result.textContent = temp.toFixed(1) + "°F"
     }
     else if(tocelsius.checked)
     {
         temp = Number(textbox.value);
         temp = (temp -32) * (5/9);
+        result.style.color = "black";
         result.textContent = temp.toFixed(1) + "°C"
     }
     else // incase nothing is selected warn user to select
